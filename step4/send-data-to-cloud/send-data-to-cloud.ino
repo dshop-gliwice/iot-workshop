67e7a6ee5421ce340898a6eaddd20d42d2947737
Adam Wałach <adam.walach@sap.com>
2016-11-17 13:25:45 +0100

    Add debug messages

--- step4/send-data-to-cloud/send-data-to-cloud.ino
+++ step4/send-data-to-cloud/send-data-to-cloud.ino
@@ -9,7 +9,7 @@ const char* password = ""; // should be empty for guest network
 String client_id = "";
 String client_secret = "";
 
-#define DHTPIN D5
+#define DHTPIN D4
 #define DHTTYPE DHT11
 DHT dht(DHTPIN, DHTTYPE);
 
@@ -41,7 +41,7 @@ void loop() {
     float t = dht.readTemperature();
     saveDTH(t, h);
   }
-  delay(30000);
+  delay(20000);
 }
 
 //check fingerprint with
@@ -49,10 +49,16 @@ void loop() {
 
 void saveDTH(float t, float h) {
   HTTPClient client;
-  client.begin("https://api.yaas.io/hybris/document/v1/iotexp/iotexp.demoapp/data/test", "DC B1 97 59 84 9D DB 76 F0 ED 7F 40 FC 0E 32 59 4F C3 AA 66");
+  client.begin("https://api.yaas.io/hybris/document/v1/iotexp/iotexp.demoapp/data/test", 
+               "DC B1 97 59 84 9D DB 76 F0 ED 7F 40 FC 0E 32 59 4F C3 AA 66");
   client.addHeader("Content-Type", "application/json");
   client.addHeader("Authorization", accessToken);
 
+  if (isnan(h) || isnan(t)){
+    Serial.println("Failed to read from DHT sensor!");
+    return;
+  }
+  
   StaticJsonBuffer<200> jsonBuffer;
   JsonObject& root = jsonBuffer.createObject();
   root["t"] = t;
@@ -64,7 +70,11 @@ void saveDTH(float t, float h) {
   if (httpCode == 201) {
     Serial.println("POST request succeeded");
   } else {
-    Serial.printf("[HTTP] POST... failed, error: %s\n", client.errorToString(httpCode).c_str());
+    Serial.printf("[HTTP] POST... failed: code: %d, error: %d\n", httpCode, client.errorToString(httpCode).c_str());
+    Serial.println("Request body: ");
+    Serial.println(json);
+    Serial.println("Request response: ");
+    Serial.println(client.getString());
   }
   Serial.flush();
   client.end();
@@ -72,10 +82,13 @@ void saveDTH(float t, float h) {
 
 void getAccessToken() {     //because of https://github.com/esp8266/Arduino/issues/2335
   HTTPClient client;
-  client.begin("https://api.yaas.io/hybris/oauth2/v1/token", "DC B1 97 59 84 9D DB 76 F0 ED 7F 40 FC 0E 32 59 4F C3 AA 66");
+  client.begin("https://api.yaas.io/hybris/oauth2/v1/token", 
+               "DC B1 97 59 84 9D DB 76 F0 ED 7F 40 FC 0E 32 59 4F C3 AA 66`");
   client.addHeader("Content-Type", "application/x-www-form-urlencoded");
 
-  int httpCode = client.POST("client_id=" + client_id + "&client_secret=" + client_secret + "&grant_type=client_credentials&scope=hybris.document_view%20hybris.document_manage");
+  int httpCode = client.POST("client_id=" + client_id + 
+                            "&client_secret=" + client_secret + 
+                            "&grant_type=client_credentials&scope=hybris.document_view%20hybris.document_manage");
 
   if (httpCode == 200) {
     String json = client.getString();
@@ -84,6 +97,7 @@ void getAccessToken() {     //because of https://github.com/esp8266/Arduino/issu
     String token_type = root["token_type"];
     String token = root["access_token"];
     accessToken = token_type + " " + token;
+    Serial.printf("Received token %s\n", token.c_str());
   } else {
     Serial.printf("Token request failed, error: %s\n", client.errorToString(httpCode).c_str());
   }
